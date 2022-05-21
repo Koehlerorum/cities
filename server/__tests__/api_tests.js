6025a837169ad6dfa9b5c8ebe033d75585d6ae07
Simo <no-reply@no-reply.com>
2022-05-21 07:44:39 +0300

    api test update

--- server/__tests__/api_tests.js
+++ server/__tests__/api_tests.js
@@ -8,14 +8,18 @@ if (process.env.CITIES_SERVER) {
     SERVER_URL=`http://localhost:3000`
 }
 
-describe('getContinents', function () {
+describe('ContinentsTests', function () {
 
-    beforeAll(async () => {
+    //Empties the current database.
+    const emptyDb = async () => {
         let res = await frisby.get(SERVER_URL);
         res.json.forEach(async continent => {
             await frisby.del(SERVER_URL + '/' + continent);
         })        
-    });
+    }
+    
+    beforeAll(emptyDb);
+    afterAll(emptyDb);
     
     it('Returns a list of all continents', function () {
         return frisby.get(SERVER_URL)
@@ -31,10 +35,10 @@ describe('getContinents', function () {
             .expect('header', 'Location', `/${continent}`)
             .then(res => {
                 return frisby.get(SERVER_URL)
+                    .expect('status', 200)
+                    .expect('header', 'Content-Type', 'application/json; charset=utf-8')
+                    .expect('json', [ continent ])
             })
-            .expect('status', 200)
-            .expect('header', 'Content-Type', 'application/json; charset=utf-8')
-            .expect('json', [ continent ]);
     });
 
     it('Add a continent missing continentName', function () {
@@ -49,7 +53,37 @@ describe('getContinents', function () {
         return frisby.post(SERVER_URL +`/?continentName=${continent}` )
             .expect('status', 409)
             .expect('json', {error: `Continent ${continent} already exists.`} )
-            .inspectBody()
     });
 
+    it('Delete an existing continent', function () {
+        const continent = "africa";
+        return frisby.del(SERVER_URL +`/${continent}` )
+            .expect('status', 200)
+    });
+
+    it('Delete non existing continent', function () {
+        const continent = "africa";
+        return frisby.del(SERVER_URL +`/${continent}` )
+            .expect('status', 404)
+            .expect('json', {error: `Continent ${continent} does not exists.`} )
+    });
+
+    it('Add multiple continents', function () {
+        const continent1 = "asia";
+        const continent2 = "africa";
+        return frisby.post(SERVER_URL +`/?continentName=${continent1}` )
+            .expect('status', 200)
+            .expect('header', 'Location', `/${continent1}`)
+            .then(res => {
+                return frisby.post(SERVER_URL +`/?continentName=${continent2}` )
+                    .expect('status', 200)
+                    .expect('header', 'Location', `/${continent2}`)
+                    .then(res => {
+                        return frisby.get(SERVER_URL)
+                            .expect('status', 200)
+                            .expect('header', 'Content-Type', 'application/json; charset=utf-8')
+                            .expect('json', [ continent1, continent2 ])
+                    })
+            });
+    });
 });
